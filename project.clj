2cfb97a038f94c93f9001ec1bc939bbecafb31e7
kesha1225 <samedov03@mail.ru>
2023-02-10 00:29:47 +0300

    sign removed

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clj-umi "0.0.1"
   :description "Umi wrapper for clojure"
-  :repositories [["github" {:url      "https://maven.pkg.github.com/RoyFractal/clj-umi"
-                            :username "private-token"
-                            :password :env/GITHUB_TOKEN}]]
+  :repositories [["github" {:url           "https://maven.pkg.github.com/RoyFractal/clj-umi"
+                            :username      "private-token"
+                            :password      :env/GITHUB_TOKEN
+                            :sign-releases false}]]
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
